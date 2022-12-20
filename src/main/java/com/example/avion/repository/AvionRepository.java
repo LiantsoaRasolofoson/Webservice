86f62592e3969a3ea88219552cca23bf9cfcef1c
LiantsoaRasolofoson <liantsoarasolofoson@gmail.com>
2022-12-20 15:42:28 +0300

    delete

--- src/main/java/com/example/avion/repository/AvionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.avion.repository;
-
-import com.example.avion.modele.Avion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AvionRepository extends JpaRepository<Avion, Integer> {
-
-}
